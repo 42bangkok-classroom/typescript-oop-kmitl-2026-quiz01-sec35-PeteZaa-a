563a33ed86abd881a898270b2b37f1e12d0befd8
PeteZaa-a <68010862@kmitl.ac.th>
2025-12-15 09:54:49 +0700

    Add files via upload

---
+++ src/q05/footbar.ts
@@ -0,0 +1,22 @@
+const input5 = +process.argv[2]
+
+if (input5 > 0) {
+    for (let i = 1; i <= input5 ; i++) {
+    if (i % 3 === 0) {
+    console.log("Foo")
+}
+    else if (i % 7 === 0) {
+        console.log("Bar")
+    }
+    else if (i % 21 === 0) {
+        console.log("FooBar")
+    }
+    else {
+        console.log(i)
+    }
+}
+}
+else {
+    console.log("Invalid Input")
+}
+
